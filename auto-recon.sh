b1a3fda031bcdbb36e116aea63735ddc0208cf23
SUJAY  ADKESAR <95465072+sujayadkesar@users.noreply.github.com>
2022-10-14 19:26:01 +0530

    Update auto-recon.sh

--- auto-recon.sh
+++ auto-recon.sh
@@ -1,21 +1,5 @@
 #! /bin/bash
 
-<<color_chart
-Black        0;30     Dark Gray     1;30
-Red          0;31     Light Red     1;31
-Green        0;32     Light Green   1;32
-Brown/Orange 0;33     Yellow        1;33
-Blue         0;34     Light Blue    1;34
-Purple       0;35     Light Purple  1;35
-Cyan         0;36     Light Cyan    1;36
-Light Gray   0;37     White         1;37
-
-example 
-red='\033[0;31m'
-echo -e "${red}this will be in red"
-https://en.wikipedia.org/wiki/ANSI_escape_code
-color_chart
-
 
 red='\e[31m'
 green='\e[32m'
